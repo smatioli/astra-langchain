74482c5be1ef331e0ba466d7be572681167b9844
Samuel Matioli <samuel.matioli@datastax.com>
2023-07-05 10:05:52 -0300

    Chat

--- connect_database.py
+++ connect_database.py
@@ -1,15 +1,16 @@
-from cassandra.cluster import Cluster
-from cassandra.auth import PlainTextAuthProvider
+# from cassandra.cluster import Cluster
+# from cassandra.auth import PlainTextAuthProvider
+# import os
 
-cloud_config= {
-  'secure_connect_bundle': '/Users/samuel.matioli/work/langchain-astra/secure-connect-astravectordb.zip'
-}
-auth_provider = PlainTextAuthProvider('XxTDXuHdCsmQftbZELQGJJXW', 'cYz6FFkIPmESmQ.PESFSR6qCjfuF4FJ1v.ZpZAmdp-5it.z9YaZoHE,BD0OGgmXowHgsnv28G6AJ8_tn9wrdikLZnxYiB3MiweCopCcno+3tM3AdWX5rKqhUdGqUMF3a')
-cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
-session = cluster.connect()
+# cloud_config= {
+#   'secure_connect_bundle': os.environ.get('ASTRA_DB_SECURE_BUNDLE_PATH')
+# }
+# auth_provider = PlainTextAuthProvider(os.environ.get('ASTRA_DB_CLIENT_ID'), os.environ.get('ASTRA_DB_CLIENT_SECRET'))
+# cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+# session = cluster.connect()
 
-row = session.execute("select release_version from system.local").one()
-if row:
-  print(row[0])
-else:
-  print("An error occurred.")
+# row = session.execute("select release_version from system.local").one()
+# if row:
+#   print(row[0])
+# else:
+#   print("An error occurred.")
